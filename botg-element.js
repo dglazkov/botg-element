e64c96e4350fc5152d77dbdf83096072f830d155
Dimitri Glazkov <dimitri@glazkov.com>
2022-01-21 21:21:12 -0800

    Draw a simple curve.

--- botg-element.js
+++ botg-element.js
@@ -5,14 +5,29 @@ class BotgElement extends HTMLElement {
     super();
 
     const shadow = this.attachShadow({ mode: 'open' });
-    const text = shadow.appendChild(document.createElement('div'));
-    const knotXs = [ 0, 5, 10, 15 ];
-    const knotYx = [ 10, 0, 5, 0 ];
+    const canvas = shadow.appendChild(document.createElement('canvas'));
+    const context = canvas.getContext('2d');
+
+    const knotXs = [ 3, 50, 100, 150 ];
+    const knotYx = [ 100, 3, 50, 3 ];
     let ks = [];
     CSPL.getNaturalKs(knotXs, knotYx, ks);
-    let x = 0;
-    let y = CSPL.evalSpline(x, knotXs, knotYx, ks);
-    text.innerText = `${y}`;
+    context.beginPath();
+    context.moveTo(knotXs[0], knotYx[0]);
+    for (let x = 1; x < 150; x+=1) {
+      let y = CSPL.evalSpline(x, knotXs, knotYx, ks);
+      context.strokeStyle = 'blue';
+      context.lineTo(x, y);
+    }
+    context.stroke();
+    for (let i = 0; i < 5; ++i) {
+      context.beginPath();
+      context.moveTo(knotXs[i], knotYx[i] - 5);
+      context.lineTo(knotXs[i], knotYx[i] + 5);
+      context.moveTo(knotXs[i] - 5, knotYx[i]);
+      context.lineTo(knotXs[i] + 5, knotYx[i]);
+      context.stroke();
+    }
   }
 }
 
