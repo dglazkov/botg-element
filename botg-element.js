168f44a4c9297ff2abd175e93a436bbeb7ede944
Dimitri Glazkov <dimitri@glazkov.com>
2022-01-24 19:50:32 -0800

    Fold Adjustable into Point.

--- botg-element.js
+++ botg-element.js
@@ -78,8 +78,8 @@ const createHtml = (points) => {
     </g>
     <path id="now" d="M 432, 3 L 430, 497" />
     <path id="curve" d="" />
-    <g id="points">${points.map((point) =>
-      `<g class="point" transform="translate(${point.x}, ${point.y})">
+    <g id="points">${points.map(() =>
+    `<g class="point"">
           <g class="hint">
             <path class="backing" d="M 0, -30 L 0, 30" />
             <path class="marker" d="M 0, -20 L 0, 20" />
@@ -87,7 +87,7 @@ const createHtml = (points) => {
           <circle class="visible" cx="0" cy="0" r="6" />
           <circle class="hit-area" cx="0" cy="0" r="30" />
       </g>`
-    )}</g>
+  )}</g>
     <g id="labels">
       <rect class="backing" x="790" y="444" width="58" height="48" />
       <text class="label" x="800" y="480">TIME</text>
@@ -100,29 +100,72 @@ const POINT_COUNT = 7
 const POINT_OFFSET = 11
 const POINT_DISTANCE = (864 / (POINT_COUNT - 1)) - 4
 const POINT_SPREAD = [...Array(POINT_COUNT)].map((_, i) => i * POINT_DISTANCE + POINT_OFFSET);
-const ADJUSTMENT_RANGE = [5, 495]; 
+const ADJUSTMENT_RANGE = [5, 495];
 
 class Point {
   constructor(x) {
     this.x = x;
     this.y = 250;
+    this.element = null;
   }
 
   attach(element, callback) {
-    new Adjustable(element, this, callback);
     this.element = element;
+    this.adjusting = false;
+    this.offset = 0;
+    this.callback = callback;
+    this.element.addEventListener('pointerdown', this.start.bind(this));
+    this.element.addEventListener('pointermove', this.adjust.bind(this));
+    this.element.addEventListener('pointerup', this.stop.bind(this));
   }
+
+  update() {
+    this.element.setAttribute('transform',
+      `translate(${this.x}, ${this.y})`);
+  }
+
+  getPointerY(evt) {
+    var ctm = this.element.ownerSVGElement.getScreenCTM();
+    return (evt.clientY - ctm.f) / ctm.d;
+  }
+
+  start(evt) {
+    this.offset = this.getPointerY(evt) - this.y;
+    this.element.setPointerCapture(evt.pointerId);
+    this.adjusting = true;
+    evt.preventDefault();
+  }
+
+  adjust(evt) {
+    if (!this.adjusting) return;
+    const y = this.getPointerY(evt) - this.offset;
+    if (y >= ADJUSTMENT_RANGE[0] && y < ADJUSTMENT_RANGE[1]) {
+      this.y = Math.round(y);
+      this.callback();
+    }
+    evt.preventDefault();
+  }
+
+  stop(evt) {
+    if (!this.adjusting) return;
+    this.element.releasePointerCapture(evt.pointerId);
+    this.adjusting = false;
+    evt.preventDefault();
+  }
+
 }
 
 class BotgElement extends HTMLElement {
+  static get observedAttributes() { return ['data']; }
+
   constructor() {
     super();
 
-    this.points = POINT_SPREAD.map(p => new Point(p));
-
     const shadow = this.attachShadow({ mode: 'open' });
-    shadow.innerHTML = createHtml(this.points);
+    shadow.innerHTML = createHtml(POINT_SPREAD);
     this.curvePath = shadow.querySelector('#curve').getAttributeNode('d');
+
+    this.points = POINT_SPREAD.map(p => new Point(p));
 
     const pathBuilder = new PathBuilder(this.curvePath, this.points);
     pathBuilder.build();
@@ -134,12 +177,8 @@ class BotgElement extends HTMLElement {
     });
   }
 
-  connectedCallback() {
-    this.readPointData(this.getAttribute('data'));
-    this.update();
-  }
-
   attributeChangedCallback(name, old, value) {
+    console.log(name, value);
     if (name != 'data') return;
     this.readPointData(value);
     this.update();
@@ -159,11 +198,9 @@ class BotgElement extends HTMLElement {
   update() {
     const pathBuilder = new PathBuilder(this.curvePath, this.points);
     pathBuilder.build();
-    this.points.forEach((point, i) => {
-      point.element.setAttribute('transform', 
-        `translate(${point.x}, ${point.y})`);
-    });
+    this.points.forEach((point) => point.update());
   }
+
 }
 
 class PathBuilder {
@@ -184,9 +221,9 @@ class PathBuilder {
       const lengthY = n.y - p.y;
       // If is end-control-point, add PI to the angle to go backward
       const angle = Math.atan2(lengthY, lengthX) + (reverse ? Math.PI : 0)
-      const length = 
-        Math.sqrt(Math.pow(lengthX, 2) + 
-        Math.pow(lengthY, 2)) * smoothing
+      const length =
+        Math.sqrt(Math.pow(lengthX, 2) +
+          Math.pow(lengthY, 2)) * smoothing
       // The control point position is relative to the current point
       const x = current.x + Math.cos(angle) * length
       const y = current.y + Math.sin(angle) * length
@@ -201,55 +238,11 @@ class PathBuilder {
       return `C ${cpsX},${cpsY} ${cpeX},${cpeY} ${point.x},${point.y}`
     }
 
-    this.attribute.value = this.points.reduce((path, point, i, a) => 
-      path.length ? 
-        `${path} ${bezierCommand(point, i, a)}` : 
+    this.attribute.value = this.points.reduce((path, point, i, a) =>
+      path.length ?
+        `${path} ${bezierCommand(point, i, a)}` :
         `M ${point.x} ${point.y}`, '');
   }
 }
-
-class Adjustable {
-  constructor(element, point, callback) {
-    this.element = element;
-    this.point = point;
-    this.adjusting = false;
-    this.offset = 0;
-    this.callback = callback;
-    this.element.addEventListener('pointerdown', this.start.bind(this));
-    this.element.addEventListener('pointermove', this.adjust.bind(this));
-    this.element.addEventListener('pointerup', this.stop.bind(this));
-  }
-
-  getPointerY(evt) {
-    var ctm = this.element.ownerSVGElement.getScreenCTM();
-    return (evt.clientY - ctm.f) / ctm.d;
-  }
-
-  start(evt) {
-    this.offset = this.getPointerY(evt) - this.point.y;
-    this.element.setPointerCapture(evt.pointerId);
-    this.adjusting = true;
-    evt.preventDefault();
-  }
-
-  adjust(evt) {
-    if (!this.adjusting) return;
-    const y = this.getPointerY(evt) - this.offset;
-    if (y >= ADJUSTMENT_RANGE[0] && y < ADJUSTMENT_RANGE[1]) {
-      this.point.y = Math.round(y);
-      console.log(this.point.y);
-      this.callback();
-    }
-    evt.preventDefault();
-  }
-
-  stop(evt) {
-    if (!this.adjusting) return;
-    this.element.releasePointerCapture(evt.pointerId);
-    this.adjusting = false;
-    evt.preventDefault();
-  }
-}
-
 
 customElements.define("botg-element", BotgElement);
