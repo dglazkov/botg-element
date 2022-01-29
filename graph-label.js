d6bc13518d60d0891f076bc47cc4f7d164deaac5
Dimitri Glazkov <dimitri@glazkov.com>
2022-01-29 11:44:15 -0800

    Make a mostly working graph-label element.

--- graph-label.js
+++ graph-label.js
@@ -4,33 +4,30 @@ const createHtml = () => {
       :host {
         box-sizing: border-box;
       }
+
       input, #backing {
-        transform: rotate(-14deg);
         border: 0;
         font-family: 'Amatic SC';
         font-size: 30px;
-        padding: 2px 5px;
+        padding: 0 6px;
+        box-sizing: border-box;
+        position: absolute;
       }
 
       input {
-        box-sizing: border-box;
         position: absolute;
-        background: transparent;
-        transition: transform 0.2s;
+        opacity: 0;
+        transition: transform 0.2s, opacity 0.2s;
       }
 
       input:focus-visible {
-        transform: rotate(0);
-        background: white;
+        opacity: 1;
       }
 
       #backing {
-        position: absolute;
+        white-space: nowrap;
         background: white;
-        color: white;
-      }
-
-      #label {
+        min-width: 40px;
       }
     </style>
     <div id="label">
@@ -40,33 +37,60 @@ const createHtml = () => {
   `
 }
 
+const createTransformCss = (rotate) => {
+  let deg = parseInt(rotate);
+  if (isNaN(deg) || deg < 0 || deg > 90) {
+    deg = 0;
+  }
+  return `
+    input:focus-visible {
+      transform: rotate(${deg}deg);
+
+    }
+
+    #label {
+      transform: rotate(-${deg}deg);
+    }
+    `;
+}
+
 class GraphLabel extends HTMLElement {
-    static get observedAttributes() { return ['title']; }
-  
-    constructor() {
-      super();
+  static get observedAttributes() { return ['title', 'rotate']; }
 
-      const shadow = this.attachShadow({ mode: 'open' });
-      shadow.innerHTML = createHtml();
+  constructor() {
+    super();
 
-      this.backing = shadow.querySelector('#backing');
-      this.input = shadow.querySelector('input');
-      this.input.addEventListener('input', () => {
-        this.adjustSize(this.input.value);
-      });
-    
-      this.adjustSize(this.input.value);
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.innerHTML = createHtml();
+
+    this.transformStyle = shadow.appendChild(document.createElement('style'));
+
+    this.text = shadow.querySelector('#backing');
+    this.input = shadow.querySelector('input');
+    this.addEventListener('input', () => {
+      this.text.textContent = this.input.value;
+    });
+  }
+
+  updateRotation(rotate) {
+    this.transformStyle.textContent = createTransformCss(rotate);
+  }
+
+  updateTitle(title) {
+    this.text.textContent = title;
+    this.input.value = title;
+  }
+
+  attributeChangedCallback(name, old, value) {
+    switch (name) {
+      case 'rotate':
+        this.updateRotation(value);
+        break;
+      case 'title':
+        this.updateTitle(value);
+        break;
     }
-
-    adjustSize(value) {
-      this.backing.textContent = value;
-      const rect = this.backing.getBoundingClientRect();
-      const width = rect.width < 50 ? 50 : rect.width - 4;
-      this.input.style.width = `${rect.width - 4}px`;
-    }
-
-    attributeChangedCallback(name, old, value) {
-    }
+  }
 }
 
 customElements.define("graph-label", GraphLabel);
